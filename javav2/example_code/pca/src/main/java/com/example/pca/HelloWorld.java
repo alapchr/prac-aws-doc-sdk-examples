65af66d94815f678febbf188719cc287bab5417d
Christiana Alapa <alapchr@amazon.com>
2025-06-23 18:13:56 +0000

    Add PCA hello world example

---
+++ javav2/example_code/pca/src/main/java/com/example/pca/HelloWorld.java
@@ -0,0 +1,13 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.example.pca;
+
+// snippet-start:[pca.java2.hello.main]
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World - PCA SDK v2 Example!");
+        System.out.println("This is a simple test example.");
+    }
+}
+// snippet-end:[pca.java2.hello.main]
