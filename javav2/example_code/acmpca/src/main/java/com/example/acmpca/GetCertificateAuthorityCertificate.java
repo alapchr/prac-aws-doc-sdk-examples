2632f62b5c3ea217c42cff8aec9ea96576f849bc
Christiana Alapa <alapchr@amazon.com>
2025-07-16 21:50:14 +0000

    Added 4 new code samples

---
+++ javav2/example_code/acmpca/src/main/java/com/example/acmpca/GetCertificateAuthorityCertificate.java
@@ -0,0 +1,70 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.example.acmpca;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.acmpca.AcmPcaClient;
+import software.amazon.awssdk.services.acmpca.model.GetCertificateAuthorityCertificateRequest;
+import software.amazon.awssdk.services.acmpca.model.GetCertificateAuthorityCertificateResponse;
+import software.amazon.awssdk.services.acmpca.model.InvalidArnException;
+import software.amazon.awssdk.services.acmpca.model.InvalidStateException;
+import software.amazon.awssdk.services.acmpca.model.ResourceNotFoundException;
+
+// snippet-start:[acmpca.java2.GetCertificateAuthorityCertificate.main]
+/**
+ * Before running this Java V2 code example, set up your development environment, including your
+ * credentials.
+ *
+ * <p>For more information, see the following documentation topic:
+ * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
+ */
+public class GetCertificateAuthorityCertificate {
+
+  public static void main(String[] args) throws Exception {
+
+    final String usage =
+        """
+
+         Usage: <regionName> <caArn>
+
+         Where:
+            regionName - The AWS region (e.g. us-east-1).
+            caArn - The ARN of the certificate authority.
+         """;
+
+    if (args.length != 2) {
+      System.out.println(usage);
+      return;
+    }
+
+    String regionName = args[0];
+    String caArn = args[1];
+
+    // Define the region for your sample.
+    Region region = Region.of(regionName);
+
+    // Create a client that you can use to make requests.
+    AcmPcaClient client = AcmPcaClient.builder().region(region).build();
+
+    // Create a request object with the Certificate Authority Arn
+    GetCertificateAuthorityCertificateRequest req =
+        GetCertificateAuthorityCertificateRequest.builder().certificateAuthorityArn(caArn).build();
+
+    // Create a result object.
+      try {
+          GetCertificateAuthorityCertificateResponse result =
+         client.getCertificateAuthorityCertificate(req);
+         
+          // Retrieve and display the certificate information.
+          String strPcaCert = result.certificate();
+          System.out.println(strPcaCert);
+          String strPCACChain = result.certificateChain();
+          System.out.println(strPCACChain);
+
+      } catch (ResourceNotFoundException | InvalidStateException | InvalidArnException e) {
+         throw e;
+      } 
+  }
+}
+// snippet-end:[acmpca.java2.GetCertificateAuthorityCertificate.main]
