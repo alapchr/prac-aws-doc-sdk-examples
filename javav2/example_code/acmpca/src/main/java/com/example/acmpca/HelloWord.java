d7fb9749a677f71a2594b34a9142018de2d340b9
Christiana Alapa <alapchr@amazon.com>
2025-08-03 18:14:09 +0000

    HelloWorld example for demo

---
+++ javav2/example_code/acmpca/src/main/java/com/example/acmpca/HelloWord.java
@@ -0,0 +1,13 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.example.acmpca;
+
+// snippet-start:[acmpca.java2.HelloWorld.main]
+public class HelloWord {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+// snippet-end:[acmpca.java2.HelloWorld.main]
